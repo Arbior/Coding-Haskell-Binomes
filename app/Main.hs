3eb214b1be94b4a444f7ae149ed711d426da0136
Arbior <deuxyeuxgrandsouverts@gmail.com>
2025-07-08 15:50:42 +0300

    Coded along the .txt

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,35 @@
 module Main (main) where
+
+import Text.Printf (printf)
 import Control.Monad (when)
 
--- Find all ways to partition a list into pairs
-allPairings :: [a] -> [[(a, a)]]
-allPairings [] = [[]]  -- Empty list has one pairing: the empty pairing
-allPairings [_] = []   -- Single element cannot be paired
-allPairings (x:xs) = do
-  -- For each element y in xs, pair x with y
-  (y, rest) <- selectOne xs
-  -- Recursively find all pairings for the remaining elements
-  pairing <- allPairings rest
-  -- Return the current pair plus the recursive pairing
-  return ((x, y) : pairing)
+-- Type definitions
+type Element = String
+type Elements = [Element]
+type Binome = (Element,Element)
+type Combination = [Binome]
+type Results = [Combination]
 
--- Helper function to select one element and return it with the rest
-selectOne :: [a] -> [(a, [a])]
-selectOne [] = []
-selectOne (x:xs) = (x, xs) : [(y, x:ys) | (y, ys) <- selectOne xs]
+-- Functions
+splitElementsAt :: Elements -> Int -> (Element,Elements)
+splitElementsAt xs i
+    | i < 0 || i >= length xs = error $ printf "Cannot split at index %i !" i
+    | otherwise = (xs !! i, take i xs ++ drop (i+1) xs)
 
--- Example usage with user input
+binomes :: Elements -> Results
+binomes [] = [[]]                                       -- If L is empty, return.
+binomes [e1] =                                          -- If L has only 1 element => error
+    error $ printf "Cannot make binome out of a single element list ! %s" e1
+binomes (l1:lr) =                                       -- Split the list in L1 = {the 1rst element} + Lr = {the rest}
+    concatMap (\i -> time lr i l1) [0 .. length lr - 1] --Do the following r times - r is the number of elements in Lr.
+    where 
+        time :: Elements -> Int -> Element -> Results
+        time l i l1' = 
+            let (li, lri) = splitElementsAt l i -- Split the list Lr => Li = {the ith element} + Lri = {the rest}
+                ri = (l1', li)                 -- Create a pair-result Ri with L1 and Li.
+                rriList = binomes lri               -- Get all possible combinations for the remaining elements
+            in map (ri :) rriList             -- Prepend ri to each combination in rriList
+
 main :: IO ()
 main = do
   putStrLn "Enter elements separated by spaces:"
@@ -30,12 +41,12 @@ main = do
                  then inputElements ++ ["Personne"]
                  else inputElements
   
-  let pairings = allPairings elements
+  let myPairings = binomes elements
+    
+  putStrLn "\nAll possible pairings :"
+  mapM_ print myPairings
   
-  putStrLn "\nAll possible pairings:"
-  mapM_ print pairings
-  
-  putStrLn $ "\nTotal number of pairings: " ++ show (length pairings)
+  putStrLn $ "\nTotal number of pairings: " ++ show (length myPairings)
   
   -- Show a message if we added "Personne"
   when (odd (length inputElements)) $
